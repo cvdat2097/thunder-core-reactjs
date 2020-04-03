51411e1bad2fe83058a596de6f295986d486ba61
cvdat2097 <cvdat2097@gmail.com>
2020-04-03 19:22:15 +0700

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
 import { MainLayout } from './layout/MainLayout';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <MainLayout>
-        <div>React App</div>
+        <h1>React App</h1>
+        <Button variant="contained" color="primary">
+          Click here
+        </Button>
       </MainLayout>
     </div>
   );
