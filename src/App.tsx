317404673d7583f7f42fb23d6f49367bc6554601
cvdat2097 <cvdat2097@gmail.com>
2020-04-03 19:10:05 +0700

    format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      React App
-    </div>
-  );
+  return <div className="App">React App</div>;
 }
 
 export default App;
